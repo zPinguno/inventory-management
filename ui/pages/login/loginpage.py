a7f27d65ece2529f64ae949224fca7eb40899efb
David <147860665+Backwards210404@users.noreply.github.com>
2026-02-07 20:40:41 +0100

    updated Login Page

--- ui/pages/login/loginpage.py
+++ ui/pages/login/loginpage.py
@@ -1,5 +1,7 @@
 from ui.pages.pageBase import PageBase
 from PyQt6.QtWidgets import *
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QColor
 
 from ui.widgetcreationhelper import createInput, createTitle, createButton
 
@@ -20,21 +22,33 @@ class LoginPage(PageBase):
         self.show()
     def createWidgets(self):
         self.fVLayout = QVBoxLayout()
+        self.fTitle = createTitle('Willkommen!',self.fVLayout)
+
         self.fUserName = createInput(self, 'Benutzername', self.fVLayout)
         self.fPassword = createInput(self, 'Passwort',self.fVLayout)
         self.fLoginButton = createButton(self, 'Anmelden',self.fVLayout)
-        self.fTitle = createTitle('Willkommen!',self.fVLayout)
         self.fCenterWidget = QWidget(self)
+
         self.prepareWidgets()
     def prepareWidgets(self):
         self.prepareStyles()
         self.fPassword.setEchoMode(QLineEdit.EchoMode.Password)
+        self.fCenterWidget.setLayout(self.fVLayout)
+        self.fVLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.fCenterWidget.setGraphicsEffect(self.setBoxShadow())
+        self.setCentralWidget(self.fCenterWidget)
     def prepareStyles(self):
         self.fVLayout.setProperty('class', 'VLayout')
         self.fUserName.setProperty('class', 'loginElements')
         self.fPassword.setProperty('class', 'loginElements')
         self.fLoginButton.setProperty('class', 'loginElements')
         self.fCenterWidget.setProperty('class', 'loginPage')
+    
+    def setBoxShadow(self):
+        shadow = QGraphicsDropShadowEffect()
+        shadow.setBlurRadius(20)  
+        shadow.setColor(QColor(0, 0, 0, 80))
+        return shadow
 
     def loadStyleSheet(self):
         styleSheet = open('./ui/pages/login/login.css').read()

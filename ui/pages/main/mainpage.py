177e7af43047057f0177c01c83027f1dfe556e9b
zPinguno <ben.diersch@icloud.com>
2026-03-05 09:40:16 +0100

    style

--- ui/pages/main/mainpage.py
+++ ui/pages/main/mainpage.py
@@ -41,9 +41,9 @@ class MainPage(PageBase):
     def createWidgets(self):
         if self.fVLayout is not None:
             return
-        self.fLogoutButton = createButton(self, 'Logout', x = 850, y = 6)
-        self.fSwitchSiteButton = createButton(self, 'Admin Seite', x = 15, y = 6)
-        self.fExportButton = createButton(self, 'Export in CSV', x = 250, y = 6)
+        self.fLogoutButton = createButton(self, 'Logout', x = 890, y = 10)
+        self.fSwitchSiteButton = createButton(self, 'Admin Seite', x = 5, y = 10)
+        self.fExportButton = createButton(self, 'Export in CSV', x = 200, y = 10)
         self.fAddItemButton = createButton(self, '+', x = self.width - 80, y = self.height - 80)
   
         self.fVLayout = QVBoxLayout()
